d75bb52f59fa4af5e828b6cfd4e08684416b6956
rupeshram013 <rupeshram00995@gmail.com>
2024-03-25 09:11:29 +0545

    project_setup_commit_01

---
+++ pages/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import request
+# Create your views here.
+
+def home(request):
+
+    return "home"
+
+
+def login(request):
+
+    return "login"
+
+
+def register(request):
+
+    return "Register"
